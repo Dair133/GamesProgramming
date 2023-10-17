5b8b828552a18de46ce1f9066c92f3a874a83a1a
Dair133 <a.ball5@nuigalway.ie>
2023-10-17 14:30:33 +0100

    DELETE
    d

--- Assets/Art/Player/Animation/Head Animations/HeadMovement.cs
+++ Assets/Art/Player/Animation/Head Animations/HeadMovement.cs
@@ -85,35 +85,51 @@ public class HeadFollowMouse : MonoBehaviour
                     //transform.localPosition = new Vector3(transform.localPosition.x + 0.05f, transform.localPosition.y + 0.05f, transform.localPosition.z);
                     transform.position = new Vector3(transform.position.x, transform.position.y - 0.03f, transform.position.z);
                 }
-                else if (angle <= -110 && angle > -165f)
+                 else if (angle < -5 && angle >= -75f)
+                 {
+                spriteRenderer.sprite = diagonalRight;
+                transform.position = new Vector3(transform.position.x + 0.06f, transform.position.y + 0.04f, transform.position.z);
+                 }
+                 else if (angle > -112.5f && angle <= -67.5f)
+                 {
+                    spriteRenderer.sprite = down;
+                    //transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
+                 }
+                else if (angle <= -112.5f && angle > -165f)
                 {
                     Debug.Log("entering diagonal left");
                     spriteRenderer.sprite = diagonalLeft;
                     transform.position = new Vector3(transform.position.x + 0.08f, transform.position.y + 0.067f, transform.position.z);
                 }
-                else if (angle > 67.5f && angle <= 112.5f)
-                {
-                    Debug.Log("down");
-                    spriteRenderer.sprite = down;
-                    //transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
-                }
-                else if (angle < -5 && angle >= -75f)
-                {
-                    spriteRenderer.sprite = diagonalRight;
-                    transform.position = new Vector3(transform.position.x + 0.06f, transform.position.y + 0.04f, transform.position.z);
-                }
-                else if ((angle >= 170 || angle <= -140.5f) || angle > 160)
+           
+             
+                else if ((angle >= 165 || angle <= -150.5f) || angle > 160)
                 {
                     Debug.Log("entering left");
                     spriteRenderer.sprite = left;
                     //transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
                 }
-                else if (angle > -112.5f && angle <= -67.5f)
+            
+                //both of these are for when the mouse is behind the character
+                else if (angle > 22f && angle <= 90f)
+                {
+
+                    spriteRenderer.sprite = right;
+            
+                 }
+                else if (angle > 90 && angle < 165f)
+                {
+                    spriteRenderer.sprite = left;
+                }
+                //statment without if statment to ensure that a down animation plays
+                //this is to compensate for if the mouse is at an angle and the character switches between differnt directions
+                //and accidentaly enters an angle which doesnt have an animation for that angle on that direction
+                //thus playing the down animation when the charater is facing up i.e. playing wrong animation for wrong direction
+                else
                 {
                     spriteRenderer.sprite = down;
-                    //transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
                 }
-            }
+        }
             if (clipName.Equals("IdleUp") || clipName.Equals("WalkUp"))
             {
                 lastMousePosition = mousePos;
@@ -127,6 +143,7 @@ public class HeadFollowMouse : MonoBehaviour
                 // Reset the local position back to the original local position
                 transform.localPosition = originalLocalPosition;
 
+                //if statments are head moving from left to right
                 if (angle > 157.5f || angle <= -157.5f)
                 {
                     spriteRenderer.sprite = left1;
@@ -150,19 +167,24 @@ public class HeadFollowMouse : MonoBehaviour
                     spriteRenderer.sprite = right1;
                     transform.position = new Vector3(transform.position.x+0.03f, transform.position.y - 0.05f, transform.position.z);
                 }
-                else if (angle > -67.5f && angle <= -22.5f)
+                //if mouse is behind character
+                else if(angle < -22.5f && angle > -90f)
                 {
-                    // Not needed as the character can't look backward
-                }
-                else if (angle > -112.5f && angle <= -67.5f)
-                {
-                    // Not needed as the character can't look backward
-                }
-                else if (angle > -157.5f && angle <= -112.5f)
-                {
-                    spriteRenderer.sprite = diagonalLeft1;
-                }
+                    spriteRenderer.sprite = right1;
+                    transform.position = new Vector3(transform.position.x + 0.03f, transform.position.y - 0.05f, transform.position.z);
             }
+                else if (angle > -160.5f && angle <= -90f)
+                {
+                    spriteRenderer.sprite = left1;
+                    transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+                 }
+                //ensure up animation always plays when facing up
+                else
+                {
+                    spriteRenderer.sprite = Up1;
+                }
+
+        }
             if (clipName.Equals("WalkLeft") || clipName.Equals("IdleLeft"))
             {
                 lastMousePosition = mousePos;
@@ -178,37 +200,47 @@ public class HeadFollowMouse : MonoBehaviour
                 transform.localPosition = originalLocalPosition;
 
                 //look up
-                if (angle > 90f && angle <= 124f)
+                if (angle > 90f && angle <= 115f)
                 {
                     spriteRenderer.sprite = Up2;
                     transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
                 }
-                else if (angle > 124f && angle <= 170f)
+                else if (angle > 115f && angle <= 162f)
                 {
                     spriteRenderer.sprite = diagonalUp1;
                 }
-                else if (angle > 170f || angle <= -170f)
+                else if (angle > 162f || angle <= -162f)
                 {
-          
-                spriteRenderer.sprite = left2;
+                    spriteRenderer.sprite = left2;
                 }
-                else if (angle > -125f && angle <= -70f)
+                else if (angle > -162f && angle <= -115f)
+                {
+                    spriteRenderer.sprite = diagonalDown2;
+                }
+                else if (angle > -115f && angle <= -90f)
                 {
                     spriteRenderer.sprite = down2;
                     transform.position = new Vector3(transform.position.x + 0.03f, transform.position.y - 0.05f, transform.position.z);
                 }
-                else if (angle > -160f && angle <= -115f)
+               
+                //when mouse behind character
+                else if(angle >90 || angle >= 0f)
                 {
-                    spriteRenderer.sprite = diagonalDown2;
+
+                    spriteRenderer.sprite = Up2;
+                //transform.position = new Vector3(transform.position.x + 0.03f, transform.position.y - 0.05f, transform.position.z);
                 }
-                //if mouse is behind character
-                else if(angle < 70f || angle >= -70f)
+                else if (angle < 0f || angle >= -90f)
                 {
-                Debug.Log("MOUSE BEHIND CHARACTER");
-                spriteRenderer.sprite = left2;
-                    //transform.position = new Vector3(transform.position.x + 0.03f, transform.position.y - 0.05f, transform.position.z);
+
+                    spriteRenderer.sprite = down2;
+                //transform.position = new Vector3(transform.position.x + 0.03f, transform.position.y - 0.05f, transform.position.z);
                 }
-            }
+                else
+                {
+                    spriteRenderer.sprite = left2;
+                }
+        }
             if (clipName.Equals("WalkRight") || clipName.Equals("IdleRight"))
             {
             lastMousePosition = mousePos;
@@ -218,52 +250,52 @@ public class HeadFollowMouse : MonoBehaviour
             );
 
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            float clampedAngle = Mathf.Clamp(angle, -180f, 180f);
             Debug.Log(angle + "ClipName Inside WalkRight" + clipName);
 
             // Reset the local position back to the original local position
             transform.localPosition = originalLocalPosition;
 
             //look up
-            if (angle < 110f && angle >= 60f)
+            if (angle < 90f && angle >= 65f)
             {
                 spriteRenderer.sprite = Up3;
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.08f, transform.position.z);
             }
             //look diagonal up
-            else if (angle <60f && angle >= 25f)
+            else if (angle <65f && angle >= 10f)
             {
                 spriteRenderer.sprite = diagonalUp3;
             }
             //look right
-            else if (angle <25f  && angle >= -25f)
+            else if (angle <15f  && angle >= -23f)//23 instead of 18 to ensure a little overlap prevents flashing between animations
             {
                 spriteRenderer.sprite = right3;
                 transform.position = new Vector3(transform.position.x, transform.position.y - 0.04f, transform.position.z);
             }
             //look diagonal down
-            else if (angle < -25f && angle > -70f)
+            else if (angle < -18f && angle > -70f)
             {
                 spriteRenderer.sprite = diagonalDown3;
                 transform.position = new Vector3(transform.position.x + 0.07f, transform.position.y - 0.04f, transform.position.z);
             }
             //look down
-            else if (angle < -60f && angle >= -110f)
+            else if (angle < -60f && angle >= -90f)
             {
                 spriteRenderer.sprite = down3;
                 transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y - 0.10f, transform.position.z);
             }
-            else if (angle > -90f && angle <= -45f)
+            else if (angle < -90f && angle >= -185)
             {
-                // Not needed as the character can't look backward
+                spriteRenderer.sprite = down3;
+                transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y - 0.10f, transform.position.z);
             }
-            else if (angle > -135f && angle <= -90f)
+            else if (angle < 180f && angle <= 90f)
             {
-                // Not needed as the character can't look backward
+                spriteRenderer.sprite = Up3;
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.08f, transform.position.z);
             }
-            //else if (angle > -180f && angle <= -135f)
-            //{
-            //    spriteRenderer.sprite = diagonalDown2;
-            //}
+         
         }
 
 
