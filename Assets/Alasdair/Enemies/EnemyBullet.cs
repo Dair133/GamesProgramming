dfa25a77ba2b7a0b277082cd17391ee12243fd5b
Dair133 <a.ball5@nuigalway.ie>
2023-12-28 20:01:20 +0000

    TEST
    sdasdasd

---
+++ Assets/Alasdair/Enemies/EnemyBullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float bulletDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ENEMY HIT");
+        // Check if the bullet has collided with an enemy
+        if (other.tag.Equals("PlayerTag"))
+        {
+            Debug.Log("Player Hit");
+
+           
+
+            // Destroy the bullet
+            Destroy(gameObject);
+        }
+    }
+}
