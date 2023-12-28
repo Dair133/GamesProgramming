c83ac2caa1ab95cf8fdc71c55a0d666a6f08a52a
Tim <T.Samoska1@nuigalway.ie>
2023-12-28 19:30:05 +0000

    UI Final Update

--- Assets/Timo/Scripts/CameraFollow.cs
+++ Assets/Timo/Scripts/CameraFollow.cs
@@ -1,12 +1,21 @@
+using System;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float offsetz;
-
     void LateUpdate()
     {
+        if (target == null)
+        { 
+            target = FindPlayer();
+        }
         transform.position = new Vector3(target.position.x, target.position.y, offsetz);
+    }
+
+    Transform FindPlayer()
+    {
+        return GameObject.FindWithTag("PlayerTag").transform;
     }
 }
